711c032a8b821e9bd30bbf139e7894e73b3128c8
Keith Pijanowski <keithpij@msn.com>
2021-04-02 07:53:57 -0400

    Cleaned up the microservice logger and the monolith logger.

--- blueprints/loggers/microservice_logger.py
+++ blueprints/loggers/microservice_logger.py
@@ -7,18 +7,28 @@ import logging
 import logging_utils as lu
 
 
-def create_microservice_logger(level):
+def create_microservice_logger(logger_name, logging_level, propagate_message=False):
     '''
-    Creates a logger with no parent and a stdout handler.
+    Creates a logger with a StreamHandler that sends messages to stdout. The logging level of
+    the logger itself is set to NOTSET. The logging level of the handler is set to the value
+    passed in via the logging_level parameter.
+
+    The logging level must numeric. Typically it is one of the contants found in the logging 
+    module (ex. logging.INFO) but it can be any number. As an example, setting it to 
+    logging.CRITICAL + 1 will turn off the handler.
+
+    Setting propagate_message to True will cause messages to be sent to parent loggers where
+    the messages will be sent to the parents handlers regardless of the level of the logger.
+    When this parameeter is false the logger will behave like a root logger.
+    
     '''
     # Create and configure the logger.
-    basic_logger = logging.getLogger('my_service_name')
+    basic_logger = logging.getLogger(logger_name)
     basic_logger.setLevel(logging.NOTSET)
-    basic_logger.parent = None
-    basic_logger.propagate = False
+    basic_logger.propagate = propagate_message
 
     # Create the handler and set the logging level.
-    stdout_handler = lu.create_stdout_handler(level)
+    stdout_handler = lu.create_stdout_handler(logging_level)
 
     # Create and add the formatter to the handler.
     stdout_handler.setFormatter(lu.create_formatter())
@@ -32,6 +42,8 @@ def create_microservice_logger(level):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--logger_name',
+                        help='Specify the name of your logger.')
     parser.add_argument('-l', '--logging_level',
                         help='Specify a logging level (NOTSET, DEBUG, INFO, WARNING, ERROR, or CRITICAL) for the basic logger.')
     parser.add_argument('-pl', '--print_levels',
@@ -42,12 +54,24 @@ if __name__ == '__main__':
                         action='store_true')
     args = parser.parse_args()
 
+    # Get the name of the logger.    
+    if args.logger_name:
+        NAME = args.logger_name
+    else:
+        NAME = __name__
+
+    # get the level to be used for the logger's handler.
     if args.logging_level:
         LEVEL = lu.convert_logging_level(args.logging_level)
         print('The current level is : ' + str(LEVEL))
-        LOGGER = create_microservice_logger(LEVEL)
+        LOGGER = create_microservice_logger(NAME, LEVEL)
         lu.log_sample_messages(LOGGER)
+    else:
+        LEVEL = logging.INFO
+
     if args.print_handlers:
         lu.print_handlers(LOGGER)
+
+    # Print logging levels.
     if args.print_levels:
         lu.print_logging_levels()
--- blueprints/loggers/monolith_logger.py
+++ blueprints/loggers/monolith_logger.py
@@ -3,22 +3,25 @@ Python Logging Blueprint for Multiple Loggers.
 '''
 import argparse
 import logging
-from logging.handlers import TimedRotatingFileHandler
-import sys
 
 import logging_utils as lu
 
 
-def create_logger_family():
-    my_svc = logging.getLogger('my_svc')
-    sales_predictions = logging.getLogger('my_svc.sales_predictions')
-    sales_reports = logging.getLogger('my_svc.sales_reports')
-    iot_telemetry = logging.getLogger('my_svc.iot_telemetry')
-    data_access = logging.getLogger('my_svc.data_access')
-    return my_svc, sales_predictions, sales_reports, iot_telemetry, data_access
+def create_monolith_logger(parent_name, children_names, level):
+    parent_logger = logging.getLogger(parent_name)
+    parent_logger.setLevel(level)
+    for child_name in children_names:
+        child_logger = logging.getLogger(parent_name + '.' + child_name)
+        child_logger.setLevel(logging.NOTSET)
+    return parent_logger
 
 
-def print_logger_relationships():
+def print_logger_relationships(parent_logger=None):
+    '''
+    If None is used in the getLogger function then the root logger will be returned.
+    '''
+    root_logger = logging.getLogger(None)
+    print(root_logger.getChild())
     my_svc, sales_predictions, sales_reports, iot_telemetry, data_access = create_logger_family()
     print(my_svc.parent.name)
     print(type(logging.getLogger('root')))
@@ -35,92 +38,29 @@ def print_logger_relationships():
         print('The parent of data_access is my_svc.')
 
 
-def create_stdout_logger(name, level):
-    '''
-    Creates a logger with no parent and a stdout handler.
-    '''
-    # Create and configure the logger.
-    my_logger = logging.getLogger('my_service_name')
-    my_logger.setLevel(logging.NOTSET)
-    my_logger.parent = None
-    my_logger.propagate = False
-
-    # Create the handler and set the logging level.
-    my_handler = logging.StreamHandler(sys.stdout)
-    my_handler.setLevel(level)
-
-    # Add the formatter to the handler.
-    my_handler.setFormatter(create_formatter())
-
-    # Add the handler to the logger.
-    my_logger.addHandler(my_handler)
-
-    return my_logger
-
-
-def create_file_logger(log_file, level):
-    '''
-    Creates a logger with no parent and a stdout handler.
-    '''
-    # Create and configure the logger.
-    logger = logging.getLogger('my_service_name')
-    logger.setLevel(logging.NOTSET)
-    logger.parent = None
-    logger.propagate = False
-
-    # Create the handler and set the logging level.
-    handler = TimedRotatingFileHandler(log_file, when='midnight')
-    handler.setLevel(level)
-
-    # Create the formatter.
-    format_string = '%(asctime)s — %(name)s — %(levelname)s — %(module)s:%(funcName)s:%(lineno)d — %(message)s'
-    formatter = logging.Formatter(format_string)
-
-    # Add the formatter to the handler.
-    handler.setFormatter(formatter)
-
-    # Add the handler to the logger.
-    logger.addHandler(handler)
-
-    return logger
-
-
-def add_stdout_handler(service_logger, level=None):
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(create_formatter())
-    if level is not None:
-        stdout_handler.setLevel(level)
-    service_logger.addHandler(stdout_handler)
-
-
-def add_file_handler(service_logger, log_file, level=None):
-    file_handler = TimedRotatingFileHandler(log_file, when='midnight')
-    file_handler.setFormatter(create_formatter())
-    if level is not None:
-        file_handler.setLevel(level)
-    service_logger.addHandler(file_handler)
-
-
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='Send logs to the specified file using a file handler.')
-    parser.add_argument('-fl', '--file_level', help='Set logging level for the file.')
-    parser.add_argument('-s', '--stdout', help='Send logs to stdout using the stream handler.', action='store_true')
-    parser.add_argument('-sl', '--stdout_level', help='Set logging level for stdout.')
-    parser.add_argument('-m', '--multiple', help='Create multiple loggers.', action='store_true')
+    parser.add_argument('-l', '--logging_level',
+                        help='Specify a logging level (NOTSET, DEBUG, INFO, WARNING, ERROR, or CRITICAL) for the basic logger.')
+    parser.add_argument('-pl', '--print_levels',
+                        help='Print all log levels and their numeric values.',
+                        action='store_true')
+    parser.add_argument('-ph', '--print_handlers',
+                        help='Print all handlers within the logger that is created.',
+                        action='store_true')
     args = parser.parse_args()
 
-    if args.file:
-        level = convert_level(args.file_level)
-        logger = create_file_logger(args.file, level)
-        log_sample_messages(logger)
-    if args.stdout:
-        level = convert_level(args.stdout_level)
-        logger = create_stdout_logger(level)
-        log_sample_messages(logger)
+    if args.logging_level:
+        LEVEL = lu.convert_logging_level(args.logging_level)
+        print('The current level is : ' + str(LEVEL))
+        PARENT_NAME = 'my_svc'
+        CHILDREN_NAMES = ['sales_predictions', 'sales_reports', 'iot_telemtry', 'data_access']
+        LOGGER = create_monolith_logger(PARENT_NAME, CHILDREN_NAMES, LEVEL)
+        lu.log_sample_messages(LOGGER)
+
+    if args.print_handlers:
+        lu.print_handlers(LOGGER)
+
     if args.print_levels:
-        print_logging_levels()
-    if args.multiple:
-        print_logger_relationships()
+        lu.print_logging_levels()
