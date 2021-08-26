1e3d14a9b71086dd4a0ee30257964c0e99411843
Keith Pijanowski <keithpij@msn.com>
2021-08-26 17:24:50 -0400

    Added Python builtin folder.

---
+++ blueprints/messaging/builtin/race.py
@@ -0,0 +1,45 @@
+'''
+This module creates a race condition.
+'''
+from threading import Thread
+import time
+
+total_value = 0
+
+def increment_total(loop):
+    global total_value
+    for _ in range(loop):
+        new_value = total_value
+        #fibonacci(new_value)
+        new_value = new_value + 1
+        total_value = new_value
+
+
+def fibonacci(sequence_size):
+    sequence = []
+    for i in range(0, sequence_size):
+        if i < 2:
+            sequence.append(i)
+            continue
+        sequence.append(sequence[i-1]+sequence[i-2])
+    return sequence_size
+
+
+def create_threads():
+    global total_value
+    loop = 1000000
+    t1 = Thread(target = increment_total, args = (loop, ))
+    t2 = Thread(target = increment_total, args = (loop, ))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+
+def main():
+    create_threads()
+    print(total_value)
+
+
+if __name__ == '__main__':
+    main()
